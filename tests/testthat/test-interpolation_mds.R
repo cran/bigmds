ecb5d6a13332962f598522ee629a750c91b5d7ef
Cristian Pachón García <cc.pachon@gmail.com>
2021-10-02 12:40:20 +0000

    version 2.0.0

--- tests/testthat/test-interpolation_mds.R
+++ tests/testthat/test-interpolation_mds.R
@@ -1,24 +1,29 @@
-test_that("Partitions for fast MDS returns a valid partition dataset", {
-  partition <- get_partitions_for_fast(n = 1000, l = 100, s_points = 5, r = 10)
+test_that("Partitions for divide-and-conquer MDS returns a valid partition dataset", {
+  partition <- get_partitions_for_interpolation(n = 1000, n_obs = 100, l = 100, r = 10)
   p <- length(partition)
-  expect_equal(p, 20)
+  expect_equal(p, 10)
 })
 
-test_that("Partitions for fast MDS returns an error when n < s_points", {
+test_that("Number of partition for interpolation MDS is 1 when n = l", {
+  partition <- get_partitions_for_interpolation(n = 1000, n_obs = 1000, l = 1000, r = 10)
+  p <- length(partition)
+  expect_equal(p, 1)
+})
+
+test_that("Number of partition for interpolation MDS is 1 when n < l", {
+  partition <- get_partitions_for_interpolation(n = 999, n_obs = 1000, l = 1000, r = 10)
+  p <- length(partition)
+  expect_equal(p, 1)
+})
+
+test_that("Partition for interpolation MDS fails when l < r", {
   expect_error(
-    get_partitions_for_fast(n = 10, l = 10, s_points = 15, r = 100),
-    "Number of rows of \"x\" must be greater than \"s_points\""
+    get_partitions_for_interpolation(n = 100, n_obs = 10, l = 10, r = 11),
+    "\"l\" must be greater than \"r\""
   )
 })
 
-test_that("Partitions for fast MDS returns an error when n*s_points/l < r", {
-  expect_error(
-    get_partitions_for_fast(n = 10, l = 100, s_points = 5, r = 100),
-    "Number of rows of \"x\" must be greater than \"r\" x \"l\"/\"s_points\""
-  )
-})
-
-test_that("fast MDS returns a valid MDS configuration when n > l", {
+test_that("interpolation MDS returns a valid MDS configuration when n > l", {
   n <- 1000
   n_cols <- 10
   l <- 100
@@ -26,7 +31,7 @@ test_that("fast MDS returns a valid MDS configuration when n > l", {
   s_points <- 2*r
   diag_mat <- sqrt(diag(c(rep(15, r), rep(1, n_cols - r))))
   x <- matrix(data = rnorm(n_cols*n), nrow = n) %*% diag_mat
-  cmds <- fast_mds(x = x, l = l, s_points = s_points, r = r)
+  cmds <- interpolation_mds(x = x, l = l, r = r)
   cmds_proc <- perform_procrustes(x = cmds$points,
                                   target = x[, 1:r],
                                   matrix_to_transform = cmds$points, 
@@ -36,7 +41,7 @@ test_that("fast MDS returns a valid MDS configuration when n > l", {
   expect_gt(min_corr, 0.9)
 })
 
-test_that("fast MDS returns a valid MDS configuration when n = l", {
+test_that("interpolation MDS returns a valid MDS configuration when n = l", {
   n <- 100
   n_cols <- 10
   l <- 100
@@ -44,7 +49,7 @@ test_that("fast MDS returns a valid MDS configuration when n = l", {
   s_points <- 2*r
   diag_mat <- sqrt(diag(c(rep(15, r), rep(1, n_cols - r))))
   x <- matrix(data = rnorm(n_cols*n), nrow = n) %*% diag_mat
-  cmds <- fast_mds(x = x, l = l, s_points = s_points, r = r)
+  cmds <- interpolation_mds(x = x, l = l, r = r)
   cmds_proc <- perform_procrustes(x = cmds$points,
                                   target = x[, 1:r],
                                   matrix_to_transform = cmds$points, 
@@ -54,7 +59,7 @@ test_that("fast MDS returns a valid MDS configuration when n = l", {
   expect_gt(min_corr, 0.9)
 })
 
-test_that("fast MDS returns a valid MDS configuration when n < l", {
+test_that("interpolation MDS returns a valid MDS configuration when n < l", {
   n <- 90
   n_cols <- 10
   l <- 100
@@ -62,7 +67,7 @@ test_that("fast MDS returns a valid MDS configuration when n < l", {
   s_points <- 2*r
   diag_mat <- sqrt(diag(c(rep(15, r), rep(1, n_cols - r))))
   x <- matrix(data = rnorm(n_cols*n), nrow = n) %*% diag_mat
-  cmds <- fast_mds(x = x, l = l, s_points = s_points, r = r)
+  cmds <- interpolation_mds(x = x, l = l, r = r)
   cmds_proc <- perform_procrustes(x = cmds$points,
                                   target = x[, 1:r],
                                   matrix_to_transform = cmds$points, 
